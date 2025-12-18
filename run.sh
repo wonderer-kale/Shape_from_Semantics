714e0a8fdb093e6dc2da05aa73174b57494a8456
dennis <lintinwei20030906@gmail.com>
2025-12-18 13:20:12 +0000

    prompt processor update

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ python ./run_tetsplatting.py \
     --views '[{"prompt":"a running dog","elevation":15,"azimuth":0},
           {"prompt":" an apple","elevation":15,"azimuth":90},
           {"prompt":"a book","elevation":15,"azimuth":180}]' \
-    -o outputs/multiview --gpus 4
+    -o outputs/multiview --gpus 6
